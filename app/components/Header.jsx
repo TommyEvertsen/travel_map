e3bfa9cf1e4df5be7c6c5795a678abd898992fcb
Tommy Evertsen <tommyevertsen52@gmail.com>
2026-02-21 22:49:14 +0100

    Added developed by and fixed issues regarding ui

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = ({ openSidebar, setIsOpenSidebar }) => {
   return (
     <div className="headerWrapper">
       <div className="header flex bg-[#6a89a7]">
-        <div className="headerButton justify-baseline items-center px-4 py-2 text-white hover:text-[#88bdf2]">
+        <div className="headerButton justify-baseline items-center px-4 py-2 text-white ">
           <button
-            className="cursor-pointer"
+            className="cursor-pointer hover:text-[#88bdf2]"
             onClick={() => setIsOpenSidebar(!openSidebar)}
           >
             <FaBars />
--- app/components/TravelMap.jsx
+++ app/components/TravelMap.jsx
@@ -110,7 +110,7 @@ export const TravelMap = ({ isTravelLayerVisible }) => {
         id="status"
         className="absolute top-4 right-4 bg-white bg-opacity-90 px-3 py-2 rounded shadow-md text-sm z-40"
       >
-        &nbsp;
+        0% - 0 of 177 countries visited
       </div>
     </div>
   );
